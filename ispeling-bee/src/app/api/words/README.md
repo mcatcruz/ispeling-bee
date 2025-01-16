060870c31f793c6af1ddb419bb6b178f8359bb36
Monica Cruz <mcatcruz@gmail.com>
2025-01-16 12:54:12 -0800

    Started README for words/config and started working on reading txt file content

--- ispeling-bee/src/app/api/words/README.md
+++ ispeling-bee/src/app/api/words/README.md
@@ -1,5 +1,5 @@
 # Structure of words folder
 * words/ → Organizes everything related to word processing.
-* process.ts → Loads and creates main words list.
+* process.ts → Loads main words list and creates words list for puzzles.
 * refresh/ → A specific action for refreshing the word list. 
 * refresh/route.ts → The file that handles the API logic.
--- ispeling-bee/src/app/api/words/process.ts
+++ ispeling-bee/src/app/api/words/process.ts
@@ -1,3 +1,49 @@
-/** 
- * Loads a main words list (txt) that 
-*/
+// This script adds new words and removes offensive words from FILIPINO_WORDS_MAIN.txt
+// FILIPINO_WORDS_MAIN.txt was created using Aspell + Filipino wordlist and Tagalog Dictionary Scraper
+// http://aspell.net/
+// https://github.com/raymelon/tagalog-dictionary-scraper/blob/master/tagalog_dict.txt
+
+import { FILE_PATHS } from "./config";
+import { promises as fs } from "fs";
+
+
+/**
+ * Reads a text file and returns an array of words.
+ *
+ * @param {string} filePath - The path to the text file.
+ * @returns {Promise<string[]>} A promise that resolves to an array of lowercase, trimmed words.
+ *                              Filters out empty lines.
+ * @throws {Error} If the file cannot be read.
+ */
+
+const getWordsFromFile = async (filePath: string): Promise<string[]> => {
+    try {
+        const txtFileContent = await fs.readFile(filePath, 'utf-8');
+
+        return txtFileContent
+            .split("\n")
+            .map((word: string) => word.trim().toLowerCase())
+            .filter((word: string) => word != "");
+
+    } catch (error) {
+        console.error(`Error reading file path: ${filePath}: `, error);
+        return [];
+    }
+}
+
+/**
+ * Reads original, added, and removed words from their respective files,
+ * processes them, and creates a combined, filtered, and sorted list of words.
+ *
+ * Steps:
+ * - Reads the three input files in parallel.
+ * - Combines original and added words.
+ * - Removes words listed in the removed words file.
+ * - Sorts the resulting list alphabetically.
+ *
+ * @param   {CreateAllWordsParams}
+ * @returns {Promise<void>} A promise that resolves to undefined if an error occurs.
+ * @throws {Error} If any file cannot be read or processed.
+ */
+
+const createFilipinoWordsPuzzle = async ()
