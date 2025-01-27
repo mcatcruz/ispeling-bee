6aa025b057f9fb5371dc328841355c444324a3b8
Monica Cruz <mcatcruz@gmail.com>
2025-01-27 15:07:46 -0800

    started filterRemovedWords unit tests

--- ispeling-bee/src/app/api/words/__tests__/filterRemovedWords.test.ts
+++ ispeling-bee/src/app/api/words/__tests__/filterRemovedWords.test.ts
@@ -1,5 +1,53 @@
-// Ensure it removes all removedWords from the input array.
-// Handle cases like:
-// No removedWords.
-// No matches between removedWords and originalAndAddedWords.
-// Edge cases (e.g., case sensitivity, special characters).
+// Mock file data
+jest.mock("fs/promises", () => ({
+    readFile: jest.fn(),
+    writeFile: jest.fn(),
+}));
+
+import * as fs from "fs/promises";
+
+import { filterRemovedWords } from "../process";
+
+describe('filterRemovedWords function', () => {
+    const mockWordsObject = { 
+        originalWords: ['ako', 'ikaw', 'puta'], 
+        addedWords: ['ako', 'siya', 'hindot'], 
+        removedWords: ['puta', 'hindot']
+    };
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+    });
+
+    it('should remove all words from removedWords', async () => {
+        const mockCombinedFileContents =
+            mockWordsObject['addedWords'].length > 0 ? [...new Set([...mockWordsObject['originalWords'], ...mockWordsObject['addedWords']])] : mockWordsObject['originalWords'];
+
+        const result = await filterRemovedWords({ originalAndAddedWords: mockCombinedFileContents, removedWords: mockWordsObject['removedWords'] });
+
+        expect(result).toEqual(['ako', 'ikaw', 'siya'])
+    })
+    
+    it('should not remove any words if removedWords is empty', async () => {
+        mockWordsObject.removedWords = [];
+
+        const mockCombinedFileContents =
+        mockWordsObject['addedWords'].length > 0 ? [...new Set([...mockWordsObject['originalWords'], ...mockWordsObject['addedWords']])] : mockWordsObject['originalWords'];
+        
+        const result = await filterRemovedWords({ originalAndAddedWords: mockCombinedFileContents, removedWords: mockWordsObject['removedWords'] });
+
+        expect(result).toEqual(mockCombinedFileContents);
+    })
+
+    it('should not remove any words if there are no matches between originalAndAddedWords and removedWords', async () => {
+        mockWordsObject.removedWords = ['gago', 'tanga'];
+
+        const mockCombinedFileContents = 
+        mockWordsObject['addedWords'].length > 0 ? [...new Set([...mockWordsObject['originalWords'], ...mockWordsObject['addedWords']])] : mockWordsObject['originalWords'];
+
+        const result = await filterRemovedWords({ originalAndAddedWords: mockCombinedFileContents, removedWords: mockWordsObject['removedWords'] });
+
+        expect(result).toEqual(mockCombinedFileContents);
+    })
+
+});
