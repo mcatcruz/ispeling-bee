17097ecbc410a63bea94dac806f4b46c0d6c24ae
Monica Cruz <mcatcruz@gmail.com>
2025-01-28 14:48:17 -0800

    Created context folder

---
+++ ispeling-bee/src/context/GameContext.ts
@@ -0,0 +1,9 @@
+// Define core state variables and their initial states:
+// correctGuesses, availableLetters, answers, middleLetter,
+// gameDate, lastGameDate, yesterdaysAvailableLetters, 
+// yesterdaysMiddleLetter, theme, pointsMessages
+
+// Use useState or useReducer + useEffect to sync state variables with local storage
+// Use setState to update state
+
+// Create a custom ReactContext hook so that we can hsare the states throughout the app
