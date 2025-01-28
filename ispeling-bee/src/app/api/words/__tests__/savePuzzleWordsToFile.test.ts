93beb8577e2916c17cc600154b3ead62acb24c51
Monica Cruz <mcatcruz@gmail.com>
2025-01-27 16:41:30 -0800

    Finished savePuzzleWordsToFile unit tests and created processPuzzleWords test file

--- ispeling-bee/src/app/api/words/__tests__/savePuzzleWordsToFile.test.ts
+++ ispeling-bee/src/app/api/words/__tests__/savePuzzleWordsToFile.test.ts
@@ -1,4 +1,53 @@
-// Test savePuzzleWordsToFile
+// Mock file data
+jest.mock("fs/promises", () => ({
+    readFile: jest.fn(),
+    writeFile: jest.fn(),
+}));
 
-// ✅ Ensure file is created and content matches expectations.
-// ✅ Handle file write errors gracefully.
+import * as fs from "fs/promises";
+
+import { savePuzzleWordsToFile } from "../process";
+
+describe('savePuzzleWordsToFile function', () => {
+    const mockWriteFile = jest.mocked(fs.writeFile);
+    const mockFilePath = 'mockFilePath.txt';
+    let mockSortedWords: string[]
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+
+        mockSortedWords = ['ako', 'ikaw', 'siya'];
+    });
+
+    it('should create a new file and content matches mockSortedWords, each string in a new line', async () => {
+
+        const logSpy = jest.spyOn(console, 'log').mockImplementation();
+
+        await expect(savePuzzleWordsToFile(mockSortedWords, mockFilePath));
+
+        expect(mockWriteFile).toHaveBeenCalledTimes(1);
+        expect(mockWriteFile).toHaveBeenCalledWith(mockFilePath, mockSortedWords.join('\n'), 'utf-8');
+        expect(logSpy).toHaveBeenCalledWith(`Puzzle words list saved to ${mockFilePath}`);
+
+        logSpy.mockRestore();
+    });
+
+    it('should handle file write errors', async () => {
+
+        const mockError = new Error("Error saving puzzle words to file");
+
+        // Mock the rejected value for fs.readFile
+        mockWriteFile.mockRejectedValueOnce(mockError);
+
+        // Spy on the console.error to validate the catch block logging
+        const errorSpy = jest.spyOn(console, 'error').mockImplementation();
+
+        // Test that the function throws the correct error
+        await expect(savePuzzleWordsToFile(mockSortedWords, mockFilePath)).rejects.toThrow(mockError);
+
+        // Validate that console.error was called with the expected arguments
+        expect(errorSpy).toHaveBeenCalledWith("Error saving puzzle words to file: ", mockError);
+        
+        errorSpy.mockRestore();
+    });
+})
