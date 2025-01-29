8d9fd378ecf89fe149760c685f55a5d161844f02
Monica Cruz <mcatcruz@gmail.com>
2025-01-28 16:51:27 -0800

    deleted duplicate tailwind and postcss config files. tested colors with boilerplate page.tsx

--- ispeling-bee/src/app/page.tsx
+++ ispeling-bee/src/app/page.tsx
@@ -15,15 +15,15 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-libre-franklin)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold-[family-name:var(--font-libre-franklin)]">
+            Get started by editing{" "} 
+            <code className=" bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold-[family-name:var(--font-libre-franklin)]">
               src/app/page.tsx
             </code>
             .
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        <h1 className="font-[family-name:var(--font-libre-franklin)] font-bold text-test">Hello World</h1>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -41,7 +41,7 @@ export default function Home() {
             Deploy now
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full border border-solid border-primary/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
